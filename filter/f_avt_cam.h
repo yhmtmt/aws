8b0912dcce349a3a41bb3343ff99d9d15a715cbf
Yohei Matsumoto <yhmtmt@yhcent.(none)>
2014-12-02 15:30:23 +0900

    correct include PvApi.h

--- filter/f_avt_cam.h
+++ filter/f_avt_cam.h
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with f_avt_cam.  If not, see <http://www.gnu.org/licenses/>. 
 
-#include "PvAPI.h"
+#include "PvApi.h"
 
 class f_avt_cam: public f_base
 {
