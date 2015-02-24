24cfe4f19c9f530cc6574989c11e0f68dcfc6314
Yohei Matsumoto <matumoto@kaiyodai.ac.jp>
2015-02-24 13:49:07 +0900

    Updating fep01 sample

---
+++ sample/fep01/fdbg.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ $# -lt 1 ]; then
+    echo "fdbg.sh <command> [<arg1> [<arg2]]"
+    exit
+fi
+
+fset fep01 cmd $1
+
+if [ $# -gt 2 ]; then
+    fset fep01 iarg1 $2    
+fi
+
+if [ $# -gt 3 ]; then
+    fset fep01 iarg2 $3
+fi
+
+fset fep01 push_cmd yes
