93136c4e94cad6b34f9e279533be8a04b11083dd
Yohei Matsumoto <yhmtmt@gmail.com>
2016-03-25 22:34:53 +0900

    New channels ch_aws1_ctrl and ch_state are created. These are the first channels implemented new channel serialization methods used in f_ch_share.

---
+++ channel/ch_aws1_ctrl.h
@@ -0,0 +1,142 @@
+#ifndef _CH_AWS1_CTRL_H_
+#define _CH_AWS1_CTRL_H_
+// Copyright(c) 2016 Yohei Matsumoto, All right reserved. 
+
+// ch_aws1_ctrl.h is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// ch_aws1_ctrl.h is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with ch_aws1_ctrl.h.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "ch_base.h"
+enum e_aws1_ctrl_src{
+  ACS_FSET, ACS_UDP, ACS_CHAN, ACS_NONE
+};
+
+// Both for transmission and reception the same packet structure is used.
+struct s_aws1_ctrl_pars{
+  // current time
+  long long tcur;
+
+  // control modes
+  bool ctrl;
+  e_aws1_ctrl_src ctrl_src;
+
+  // control output
+  unsigned char rud;
+  unsigned char meng;
+  unsigned char seng;
+
+  // aws control input (from aws)
+  unsigned char rud_aws;
+  unsigned char meng_aws;
+  unsigned char seng_aws;
+
+  // aws remote controller's input (from adc)
+  unsigned char rud_rmc;
+  unsigned char meng_rmc;
+  unsigned char seng_rmc;
+
+  // rudder angle 
+  unsigned char rud_sta;     // from adc
+  unsigned char rud_sta_out; // to digi-pot
+
+  // remote controller's values corresponding positions 
+  unsigned char meng_max_rmc; 
+  unsigned char meng_nuf_rmc;
+  unsigned char meng_nut_rmc;
+  unsigned char meng_nub_rmc;
+  unsigned char meng_min_rmc;
+
+  unsigned char seng_max_rmc;
+  unsigned char seng_nuf_rmc;
+  unsigned char seng_nut_rmc;
+  unsigned char seng_nub_rmc;
+  unsigned char seng_min_rmc;
+
+  unsigned char rud_max_rmc;
+  unsigned char rud_nut_rmc;
+  unsigned char rud_min_rmc;
+
+  unsigned char rud_sta_max;
+  unsigned char rud_sta_nut;
+  unsigned char rud_sta_min;
+
+  // Threashold values of digital potentiometer's
+  unsigned char meng_max;
+  unsigned char meng_nuf;
+  unsigned char meng_nut;
+  unsigned char meng_nub;
+  unsigned char meng_min;
+
+  unsigned char seng_max;
+  unsigned char seng_nuf;
+  unsigned char seng_nut;
+  unsigned char seng_nub;
+  unsigned char seng_min;
+
+  unsigned char rud_max;
+  unsigned char rud_nut;
+  unsigned char rud_min;
+
+  unsigned char rud_sta_out_max;
+  unsigned char rud_sta_out_nut;
+  unsigned char rud_sta_out_min;
+
+  // success flag
+  bool suc;
+
+  s_aws1_ctrl_pars();
+};
+
+class ch_aws1_ctrl: public ch_base
+{
+ protected:
+  s_aws1_ctrl_pars pars;
+ public:
+ ch_aws1_ctrl(const char * name):ch_base(name)
+  {
+  }
+
+  void set_pars(const s_aws1_ctrl_pars & _pars){
+    lock();
+    pars = _pars;
+    unlock();
+  }
+
+  void get_pars(s_aws1_ctrl_pars & _pars){
+    lock();
+    _pars = pars;
+    unlock();
+  }
+
+  virtual size_t get_dsize()
+  {
+    return sizeof(s_aws1_ctrl_pars);
+  }
+
+  virtual size_t write_buf(const char * buf)
+  {
+    lock();
+    memcpy((void*)&pars, (void*)buf, sizeof(s_aws1_ctrl_pars));
+    unlock();
+    return sizeof(s_aws1_ctrl_pars);
+  }
+
+  virtual size_t read_buf(char * buf)
+  {
+    lock();
+    memcpy((void*)buf, (void*)&pars, sizeof(s_aws1_ctrl_pars));
+    unlock();
+    return sizeof(s_aws1_ctrl_pars);
+  }
+};
+
+#endif
---
+++ channel/ch_state.h
@@ -0,0 +1,101 @@
+#ifndef _CH_STATE_H_
+#define _CH_STATE_H_
+// Copyright(c) 2016 Yohei Matsumoto, All right reserved. 
+
+// ch_state.h is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// ch_state.h is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with ch_state.h.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "ch_base.h"
+
+class ch_state: public ch_base
+{
+ protected:
+  float r, p, y; // roll pitch yaw
+  float lon, lat, alt; // longitude, latitude, altitude
+ public:
+ ch_state(const char * name): ch_base(name), r(0), p(0), y(0),
+    lon(0), lat(0), alt(0)
+    {
+    }
+
+  void set_attitude(const float _r, const float _p, const float _y)
+  {
+    lock();
+    r = _r; 
+    p = _p;
+    y = _y;
+    unlock();
+  }
+
+  void set_position(const float _lat, const float _lon, const float _alt)
+  {
+    lock();
+    lat = _lat;
+    lon = _lon;
+    alt = _alt;
+    unlock();
+  }
+
+  void get_attitude(float & _r, float & _p, float & _y)
+  {
+    lock();
+    _r = r;
+    _p = p;
+    _y = y;
+    unlock();
+  }
+
+  void get_position(float & _lat, float & _lon, float & _alt)
+  {
+    lock();
+    _lat = lat;
+    _lon = lon;
+    _alt = alt;
+    unlock();
+  }
+
+  virtual size_t get_dsize()
+  {
+    return sizeof(float) * 6;
+  }
+  
+  virtual size_t write_buf(const char *buf)
+  {
+    lock();
+    const float * ptr = (const float*) buf;
+    r = ptr[0];
+    p = ptr[1];
+    y = ptr[2];
+    lat = ptr[3];
+    lon = ptr[4];
+    alt = ptr[5];
+    unlock();
+    return sizeof(float) * 6;
+  }
+
+  virtual size_t read_buf(char * buf)
+  {
+    lock();
+    float * ptr =  (float*) buf;
+    ptr[0] = r;
+    ptr[1] = p;
+    ptr[2] = y;
+    ptr[3] = lat;
+    ptr[4] = lon;
+    ptr[5] = alt;
+    unlock();
+    return sizeof(float) * 6;
+  }
+};
+
+#endif
