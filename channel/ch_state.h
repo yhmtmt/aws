53cafcd091795c8adefdc874918b2a1d584fee35
Yohei Matsumoto <matumoto@kaiyodai.ac.jp>
2016-04-21 21:59:38 +0900

    ENU rotation matrix and ECEF is calculated in set_position. And stored as members of ch_state.

--- channel/ch_state.h
+++ channel/ch_state.h
@@ -15,27 +15,31 @@
 // You should have received a copy of the GNU General Public License
 // along with ch_state.h.  If not, see <http://www.gnu.org/licenses/>.
 
+#include "../util/aws_coord.h"
+
 #include "ch_base.h"
 
 class ch_state: public ch_base
 {
  protected:
-  float r, p, y; // roll(deg), pitch(deg), yaw(deg)
+  float roll, pitch, yaw; // roll(deg), pitch(deg), yaw(deg)
   float lon, lat, alt, galt; // longitude(deg), latitude(deg), altitude(m), geoid altitude(m)
+  float x, y, z; // ecef coordinate
+  Mat R; // Rotation matrix for ENU transformation
   float cog, sog; // Course over ground(deg), Speed over ground (kts)
   float depth; // water depth
  public:
- ch_state(const char * name): ch_base(name), r(0), p(0), y(0),
-    lon(0), lat(0), alt(0), galt(0), cog(0), sog(0), depth(0)
+ ch_state(const char * name): ch_base(name), roll(0), pitch(0), yaw(0),
+    lon(0), lat(0), alt(0), galt(0), x(0), y(0), z(0), cog(0), sog(0), depth(0)
     {
     }
 
   void set_attitude(const float _r, const float _p, const float _y)
   {
     lock();
-    r = _r; 
-    p = _p;
-    y = _y;
+    roll = _r; 
+    pitch = _p;
+    yaw = _y;
     unlock();
   }
 
@@ -46,6 +50,8 @@ class ch_state: public ch_base
     lon = _lon;
     alt = _alt;
 	galt = _galt;
+	getwrldrot(lat, lon, R);
+	bihtoecef(lat, lon, alt, x, y, z);
     unlock();
   }
 
@@ -66,9 +72,9 @@ class ch_state: public ch_base
   void get_attitude(float & _r, float & _p, float & _y)
   {
     lock();
-    _r = r;
-    _p = p;
-    _y = y;
+    _r = roll;
+    _p = pitch;
+    _y = yaw;
     unlock();
   }
 
@@ -80,6 +86,20 @@ class ch_state: public ch_base
     _alt = alt;
 	_galt = galt;
     unlock();
+  }
+
+  void get_position_ecef(float & _x, float & _y, float & _z)
+  {
+	  lock();
+	  _x = x;
+	  _y = y;
+	  _z = z;
+	  unlock();
+  }
+
+  const Mat & get_enu_rotation()
+  {
+	  return R;
   }
 
   void get_velocity(float & _cog, float & _sog)
@@ -99,48 +119,58 @@ class ch_state: public ch_base
 
   virtual size_t get_dsize()
   {
-    return sizeof(float) * 10;
+    return sizeof(float) * 13 + sizeof(double) * 9;
   }
   
   virtual size_t write_buf(const char *buf)
   {
     lock();
     const float * ptr = (const float*) buf;
-    r = ptr[0];
-    p = ptr[1];
-    y = ptr[2];
+    roll = ptr[0];
+    pitch = ptr[1];
+    yaw = ptr[2];
     lat = ptr[3];
     lon = ptr[4];
     alt = ptr[5];
     galt = ptr[6];
-    cog = ptr[7];
-    sog = ptr[8];
-    depth = ptr[9];
+	x = ptr[7];
+	y = ptr[8];
+	z = ptr[9];
+    cog = ptr[10];
+    sog = ptr[11];
+    depth = ptr[12];
+	const double * dptr = (const double*)(ptr + 13);
+	memcpy((void*)R.data, (void*) dptr, sizeof(double) * 9);
     unlock();
-    return sizeof(float) * 10;
+    return get_dsize();
   }
 
   virtual size_t read_buf(char * buf)
   {
     lock();
     float * ptr =  (float*) buf;
-    ptr[0] = r;
-    ptr[1] = p;
-    ptr[2] = y;
+    ptr[0] = roll;
+    ptr[1] = pitch;
+    ptr[2] = yaw;
     ptr[3] = lat;
     ptr[4] = lon;
     ptr[5] = alt;
     ptr[6] = galt;
-    ptr[7] = cog;
-    ptr[8] = sog;
-    ptr[9] = depth;
+	ptr[7] = x;
+	ptr[8] = y;
+	ptr[9] = z;
+    ptr[10] = cog;
+    ptr[11] = sog;
+    ptr[12] = depth;
+	double * dptr = (double*) (ptr + 13);
+	memcpy((void*)dptr, (void*) R.data, sizeof(double) * 9);
     unlock();
-    return sizeof(float) * 10;
+    return get_dsize();
   }
   
   virtual void print(ostream & out)
   {
-    out << "channel " << m_name << " RPY= " << r << "," << p << "," << y 
+    out << "channel " << m_name << " RPY= " << roll << "," << pitch << "," << yaw 
 	<< " pos= " << lat << "," << lon << "," << alt << endl;
   }
 };
