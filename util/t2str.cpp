3a0ba60930acc3b05395d1835eedf8d08abc2eee
Yohei Matsumoto <yhmtmt@gmail.com>
2016-04-29 22:43:04 +0900

    Utility program t2str is added under ./util. This program converts awstime to string.

---
+++ util/t2str.cpp
@@ -0,0 +1,32 @@
+#include <time.h>
+#include <stdlib.h>
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+#include "c_clock.h"
+
+int main(int argc, char ** argv)
+{
+  if(argc != 2){
+    cout << "Usage: t2str <aws time>" << endl;
+    return 0;
+  }
+
+  long long t = atol(argv[1]);
+  t /= MSEC;
+  tmex tm;
+  gmtimeex(t, tm);
+  printf("[%s %s %02d %02d:%02d:%02d.%03d %d] ", 
+	   getWeekStr(tm.tm_wday), 
+	   getMonthStr(tm.tm_mon),
+	   tm.tm_mday,
+	   tm.tm_hour,
+	   tm.tm_min,
+	   tm.tm_sec,
+	   tm.tm_msec,
+	   tm.tm_year + 1900);
+
+  return true;
+};
