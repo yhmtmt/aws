ea20e9547c6cd5c98e4e568b879656b39b868b44
Yohei Matsumoto <yhmtmt@gmail.com>
2018-10-04 15:27:58 +0900

    Updating setup_awsenv_ubuntu1804.sh.

--- setup_awsenv_ubuntu1804.sh
+++ setup_awsenv_ubuntu1804.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt update
 sudo apt install -y libglfw3 libglfw3-dev
-sudo apt install -y libfreeglut3 libfreeglut3-dev
+sudo apt install -y freeglut3 freeglut3-dev
 sudo apt install -y libglm-dev
 sudo apt install -y libeigen3-dev
 sudo apt install -y libgstreamer-plugins*-dev
