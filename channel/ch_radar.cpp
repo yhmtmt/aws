d1b3f1e8c0caa58d32f769aee050aac49775d6d7
Yohei Matsumoto <yhmtmt@gmail.com>
2019-09-19 21:37:22 +0900

    GPL lines are inserted to ch_radar.cpp/h

--- channel/ch_radar.cpp
+++ channel/ch_radar.cpp
@@ -1,3 +1,17 @@
+// Copyright(c) 2019 Yohei Matsumoto, All right reserved. 
+
+// ch_radar.cpp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// ch_radar.cpp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with ch_radar.cpp.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <iostream>
 #include <vector>
--- channel/ch_radar.h
+++ channel/ch_radar.h
@@ -1,5 +1,21 @@
 #ifndef CH_RADAR_H
 #define CH_RADAR_H
+// Copyright(c) 2019 Yohei Matsumoto, All right reserved. 
+
+// ch_radar.h is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// ch_radar.h is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with ch_radar.h.  If not, see <http://www.gnu.org/licenses/>.
+
+
 #include "ch_base.h"
 #include "../filter/f_radar_srcs/socketutil.h"
 #include "../filter/f_radar_srcs/garminxhd.h"
